67709f2d88f09d220ee9f114a7501521f82c5b3a
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-15 15:38:41 +0500

    Added cars images

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const cars = {
             price: "2200933",
             colors: ["red", "black", "white"],
             modal: "2504",
+            img : './assets/civic.jpg'
         },
         hondaCity:{
             name: "Honda City",
             price: "2200923233",
             colors: ["gray", "black", "white"],
-            modal: "2004"
+            modal: "2004",
+            img : './assets/honda-city.png'
         }
 
     },
@@ -19,28 +21,31 @@ const cars = {
             name: "Move",
             price: "1200933",
             colors: ["red", "blue", "white"],
-            modal: "2000"
+            modal: "2000",
+            img : './assets/move.webp'
         },
         mira:{
             name: "Mira",
             price: "1093233",
             colors: ["red", "gray", "yellow"],
-            modal: "2024"
+            modal: "2024",
+            img : './assets/mira.jpeg'
         }
     },
      corolla:{
-
         CorollaAltis:{
             name: "Carolla Altis",
             price: "2000000",
             colors: ["white"],
-            modal: "1964"
+            modal: "1964",
+            img : './assets/altis.webp'
         },
         yaris:{
             name: "Yaris",
             price: 450000,
             colors: [ "gray", 'red'],
-            modal: "2022"
+            modal: "2022",
+            img : './assets/yaris.png'
         }
 
     },
