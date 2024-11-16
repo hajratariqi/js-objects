3b4ddb6bb35d82e57c0d60f355da99860cb3c7cd
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-11-17 00:24:57 +0500

    comment code

---
+++ task.js
@@ -0,0 +1,43 @@
+// let carCompany = {
+//     honda: {
+//         city: {
+//             name: 'Honda City',
+//             price: '60M', 
+//             image: 'https://cache1.pakwheels.com/system/car_generation_pictures/6425/original/Honda_City_Front.jpg?1651424945'
+//         }},
+//     toyota: {
+//         fortuner: {
+//             name: 'Toyota Fortuner',
+//             price: '40M',
+//             image: 'https://cache4.pakwheels.com/system/car_generation_pictures/7335/original/Fortuner_-_PNG.png?1677568997'
+//         }},
+// }
+
+// let campanys = document.getElementById('campany')
+
+// for (let key in carCompany) {
+//     console.log(carCompany[key])
+//     campanys.innerHTML += `<option selected>${key}</option>`
+// }
+
+
+// let carsModal = document.getElementById('carsModal')
+
+// for (let key in carCompany) {
+//     console.log(carCompany[key])
+//     campanys.innerHTML += `<option selected>${key}</option>`
+// }
+
+
+// function selectCompany(type) {
+//     console.log('selectCompany test', type.value)
+// }
+
+// function selectModal(type) {
+//     console.log('selectModal test', type.value)
+// }
+
+// function searchCar(type) {
+//     console.log('selectModal test', type.value)
+// }
+
